f2ad1043b2407bd59d89b01d1213df8a41f0d696
Georgemuchir <168327411+Georgemuchir@users.noreply.github.com>
2024-07-07 23:30:19 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,11 @@
 //selectors
-const addButtonEl = document.querySelector("#add to cart");
-const clearListButton = document.querySelector("#clear cart");
+// const addButtonEl = document.querySelector("#add to cart");
+// const clearListButton = document.querySelector("#clear cart");
+let removeCartItemButtons=document.GetElementByClassName('action-button')
+console.log(removeCartItemButtons)
+for (var i=0; i<removeCartItemButtons;i++){
+    let button=removeCartItemButtons[i]
+    button.addEventListener('click', function(event){
+        let buttonclicked = event.target})
+    }
 
